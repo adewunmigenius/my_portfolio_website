461a44e9cc60a776e60fca56d900c882ace1995f
OJO-Ahmed <adewunmigenius@gmail.com>
2017-07-18 18:01:21 +0100

    js file for modifying page document

---
+++ js/script.js
@@ -0,0 +1,26 @@
+var $overlay = $('<div id="overlay"></div>');
+var $image = $('<img>');
+var $caption = $('<p></p>');
+
+//Add image to overlay
+$overlay.append($image);
+$overlay.append($caption);
+
+//Add overlay to body of app
+$('body').append($overlay);
+
+$('#gallery a').click(function(event){
+	event.preventDefault();
+	//get anchor link href
+	var imglocation = $(this).attr('href');
+	//update the overlay with image
+	$image.attr('src', imglocation);
+	//show overlay
+	$overlay.show();
+	//get caption from paragraph text
+	var captionText= $(this).children('p').html();
+	$caption.text(captionText);
+});
+$overlay.click(function(){
+	$(this).hide();
+});
